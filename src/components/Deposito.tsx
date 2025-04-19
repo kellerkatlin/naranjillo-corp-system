c66806a5bdba2d75587dd823c01c642c7f59edf7
kellerkatlin <kellerkatlin.k@gmail.com>
2025-04-19 12:42:05 -0500

    fix: add overflow-hidden class to Deposito component for proper display

--- src/components/Deposito.tsx
+++ src/components/Deposito.tsx
@@ -5,11 +5,13 @@ interface DepositoProps {
 export default function Deposito({ porcentaje, nombre }: DepositoProps) {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-20 h-24 border-2 rounded-b-md border-black relative">
+      <div className="w-20 h-24 border-2 rounded-b-md border-black relative overflow-hidden">
         <div
-          className="absolute bottom-0 left-0 w-full bg-[#29f151] text-black text-center font-bold"
+          className="absolute bottom-0 left-0 w-full bg-[#29f151]"
           style={{ height: `${porcentaje}%` }}
-        >
+        />
+
+        <div className="absolute inset-0 flex items-center justify-center z-10 text-black font-bold">
           {porcentaje} %
         </div>
       </div>
