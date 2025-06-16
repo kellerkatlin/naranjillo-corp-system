573e571a4f46c348e3cd8e0fd8ed76caa8916aab
kellerkatlin <kellerkatlin.k@gmail.com>
2025-06-16 00:48:10 -0500

    fix: enhance CardJava component styling for improved user interaction

--- src/components/CardJava.tsx
+++ src/components/CardJava.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Card, CardContent } from "./ui/card";
 import { JavaRespose } from "@/types/java";
-import { Pencil } from "lucide-react";
-import { Button } from "./ui/button";
 
 interface CardJavaProps {
   readonly java?: JavaRespose;
@@ -13,7 +11,7 @@ export default function CardJava({ java, onClickEdit }: CardJavaProps) {
   return (
     <Card
       onClick={onClickEdit}
-      className="w-36 h-36 border-black bg-green-400 items-center justify-center relative"
+      className="w-36 h-36 border-black bg-green-400 items-center justify-center relative cursor-pointer hover:scale-105"
     >
       <CardContent className="p-2 flex flex-col justify-center items-center text-center">
         <div className="font-semibold text-white text-lg">{java?.nombre}</div>
